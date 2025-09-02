8edfe3f041f1b47811291444005d26e1b38a2c18
Luis Guilherme Passos Ramos <20222tadssaj0015@ifba.edu.br>
2025-09-02 19:29:26 -0300

    Feat: Update EventRepository to extend CrudRepository

--- workshop-backend/workshop-core/src/main/java/br/ifba/ads/workshop/core/domain/repositories/contracts/EventRepository.java
+++ workshop-backend/workshop-core/src/main/java/br/ifba/ads/workshop/core/domain/repositories/contracts/EventRepository.java
@@ -2,6 +2,5 @@ package br.ifba.ads.workshop.core.domain.repositories.contracts;
 
 import br.ifba.ads.workshop.core.domain.models.Event;
 
-public interface EventRepository{
-    Event save (Event event);
+public interface EventRepository extends CrudRepository<Event> {
 }
