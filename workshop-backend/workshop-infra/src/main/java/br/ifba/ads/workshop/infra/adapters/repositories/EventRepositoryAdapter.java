56f966c2814fc1bfe2c8de346637c491ba6f4f5b
Luis Guilherme Passos Ramos <20222tadssaj0015@ifba.edu.br>
2025-09-02 19:30:36 -0300

    feat: create EventRepositoryAdapter

---
+++ workshop-backend/workshop-infra/src/main/java/br/ifba/ads/workshop/infra/adapters/repositories/EventRepositoryAdapter.java
@@ -0,0 +1,22 @@
+package br.ifba.ads.workshop.infra.adapters.repositories;
+
+import br.ifba.ads.workshop.core.domain.models.Event;
+import br.ifba.ads.workshop.core.domain.repositories.contracts.EventRepository;
+import br.ifba.ads.workshop.infra.mappers.BaseEntityMapper;
+import br.ifba.ads.workshop.infra.mappers.EventEntityMapper;
+import br.ifba.ads.workshop.infra.persistence.entities.event.EventEntity;
+import br.ifba.ads.workshop.infra.persistence.repositories.JpaBaseRepository;
+import br.ifba.ads.workshop.infra.persistence.repositories.JpaEventRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public final class EventRepositoryAdapter extends CrudRepositoryAdapter<Event, EventEntity> implements EventRepository {
+
+    public EventRepositoryAdapter(
+            JpaEventRepository repository,
+            EventEntityMapper mapper
+    ) {
+        super(repository, mapper);
+    }
+}
+
