86bfe9648ca0c151d46058aeb76cdb65665950df
Luis Guilherme Passos Ramos <20222tadssaj0015@ifba.edu.br>
2025-09-02 19:29:26 -0300

    Feat: Update JpaEventRepository to extend JpaBaseRepository

--- workshop-backend/workshop-infra/src/main/java/br/ifba/ads/workshop/infra/persistence/repositories/JpaEventRepository.java
+++ workshop-backend/workshop-infra/src/main/java/br/ifba/ads/workshop/infra/persistence/repositories/JpaEventRepository.java
@@ -1,9 +1,7 @@
 package br.ifba.ads.workshop.infra.persistence.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import br.ifba.ads.workshop.infra.persistence.entities.event.EventEntity;
 
-import br.ifba.ads.workshop.core.domain.models.Event;
-
-public interface JpaEventRepository extends JpaRepository<Event, Long> {
-    
+public interface JpaEventRepository extends JpaBaseRepository<EventEntity> {
 }
+
