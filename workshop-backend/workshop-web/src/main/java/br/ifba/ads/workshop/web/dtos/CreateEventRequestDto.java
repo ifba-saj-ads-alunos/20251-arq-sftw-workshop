17db9fc5e8aaa6e19e821f12d243c52415b21be0
Luis Guilherme Passos Ramos <20222tadssaj0015@ifba.edu.br>
2025-09-02 19:30:36 -0300

    feat: create event request DTO

---
+++ workshop-backend/workshop-web/src/main/java/br/ifba/ads/workshop/web/dtos/CreateEventRequestDto.java
@@ -0,0 +1,16 @@
+package br.ifba.ads.workshop.web.dtos;
+
+public record CreateEventRequestDto(
+        String titulo,
+        String descricao,
+        String categoria,
+        String dataInicio, // yyyy-MM-dd
+        String dataFim,    // yyyy-MM-dd
+        String vagas,
+        String palestrante, // not persisted yet
+        String curriculo,   // not persisted yet
+        String localidade,  // Remota | Presencial
+        String link,        // for Remota
+        String sala         // for Presencial
+) {}
+
