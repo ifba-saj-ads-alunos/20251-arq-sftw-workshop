986787cc12c5295cb487ef0472e52d4f3f0becbb
Luis Guilherme Passos Ramos <20222tadssaj0015@ifba.edu.br>
2025-09-03 22:53:33 -0300

    Refactor: Adapt Spring Transactional Manager

--- workshop-backend/workshop-infra/src/main/java/br/ifba/ads/workshop/infra/adapters/SpringTransactionalManager.java
+++ workshop-backend/workshop-infra/src/main/java/br/ifba/ads/workshop/infra/adapters/SpringTransactionalManager.java
@@ -1,22 +1,22 @@
-    package br.ifba.ads.workshop.infra.adapters;
+package br.ifba.ads.workshop.infra.adapters;
 
-    import br.ifba.ads.workshop.core.application.gateways.TransactionManagerGateway;
-    import org.springframework.stereotype.Component;
-    import org.springframework.transaction.annotation.Transactional;
+import br.ifba.ads.workshop.core.application.gateways.TransactionManagerGateway;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-    import java.util.function.Supplier;
+import java.util.function.Supplier;
 
-    @Component
-    public class SpringTransactionalManager implements TransactionManagerGateway {
-        @Override
-        @Transactional
-        public <T> T doInTransaction(Supplier<T> action) {
-            return action.get();
-        }
-
-        @Override
-        @Transactional
-        public void doInTransaction(Runnable action) {
-            action.run();
-        }
+@Component
+public class SpringTransactionalManager implements TransactionManagerGateway {
+    @Override
+    @Transactional
+    public <T> T doInTransaction(Supplier<T> action) {
+        return action.get();
     }
+
+    @Override
+    @Transactional
+    public void doInTransaction(Runnable action) {
+        action.run();
+    }
+}
